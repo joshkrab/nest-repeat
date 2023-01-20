cc29cdc67aeb17607b1af33f6138432f47da851e
Ihor Neizvestnyi <ihor.neizvestnyi@novait.local>
2023-01-20 18:01:31 +0200

    pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'ella.db.elephantsql.com',
+      port: 5432,
+      username: 'ssxnhuvu',
+      password: 'Pf7Cpc8k1bpJH4P9NgGHeCjjnxGY2Thh',
+      database: 'ssxnhuvu',
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
 })
 export class AppModule {}
