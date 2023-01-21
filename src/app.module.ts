bb9665a4a62f06e6a7a93be754916919eec81400
Ihor Neizvestnyi <ihor.neizvestnyi@novait.local>
2023-01-21 15:47:35 +0200

    dddsss

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'ella.db.elephantsql.com',
-      port: 5432,
-      username: 'ssxnhuvu',
-      password: 'Pf7Cpc8k1bpJH4P9NgGHeCjjnxGY2Thh',
-      database: 'ssxnhuvu',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       models: [],
       autoLoadModels: true,
     }),
+    UsersModule,
   ],
 })
 export class AppModule {}
